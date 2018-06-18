5936e117785ea9cdba326aa67b5db7fe33fb8d64
gux12 <gx199492@qq.com>
2018-06-19 01:57:37 +0800

    修改Reg为public readonly属性防止修改

--- typings/egg-redis/index.d.ts
+++ typings/egg-redis/index.d.ts
@@ -1,4 +1,5 @@
 import * as IORedis from "ioredis"
+// TODO 向ts-helper增加CusCommand的自动生成代码
 interface CusCommand {
     isGlobalLock(key: string, callback: (err: Error, res: number) => void): void;
     isGlobalLock(key: string): Promise<number>;
