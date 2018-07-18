a5c243b544a41ea39fbd7b63bbc7b9be4b379366
florence <misterapptracy@gmail.com>
2018-07-18 15:38:20 +0800

    完善代码规范

--- migrations/20180617173408-addTableUser.ts
+++ migrations/20180617173408-addTableUser.ts
@@ -2,29 +2,29 @@
 import * as sequelize from 'sequelize';
 import ModelBizAttributes from '../app/core/base/modelBizAttributes';
 module.exports = {
-  async up(queryInterface: sequelize.QueryInterface, Sequelize: sequelize.SequelizeStatic) {
+  async up(queryInterface: sequelize.QueryInterface, sequelize: sequelize.SequelizeStatic) {
     return [
       await queryInterface.createTable('user', {
         ...ModelBizAttributes,
         username: {
-          type: Sequelize.STRING(32),
+          type: sequelize.STRING(32),
           allowNull: false,
           comment: '应用的唯一key',
         },
         nickname: {
-          type: Sequelize.STRING(32),
+          type: sequelize.STRING(32),
           allowNull: false,
           defaultValue: '',
           comment: '昵称',
         },
         password: {
-          type: Sequelize.STRING(64),
+          type: sequelize.STRING(64),
           allowNull: false,
           defaultValue: '',
           comment: '密码',
         },
         headImageUrl: {
-          type: Sequelize.STRING(256),
+          type: sequelize.STRING(256),
           allowNull: true,
           comment: '头像地址',
         },
